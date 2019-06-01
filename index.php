d00fba14a652386b965e58a3e7821385f10ec354
Julmar <julmarfigueroa@gmail.com>
2019-06-01 03:48:25 -0400

    Add - php route to index

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    // $email_from = 'thegame10182@yahoo.com';
+    $email_from = 'thegame10182@yahoo.com';
     $email_subject = "New Form Submission";
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                         "User Message: $message.\n";
 
     $to = "thegame10182@yahoo.com";
-    $headers = "From: $visitor_email \r\n";
+    $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
     header("Location: index.html");
